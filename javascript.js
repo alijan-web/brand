3389316165b8fccf81efb3cdeb9c8e5d21d6e53e
Ali Jan <alijaan.web@gmail.com>
2023-11-17 23:13:58 -0800

    new files add

---
+++ javascript.js
@@ -0,0 +1,51 @@
+// // var a = 0;
+// // var b = prompt("TYPE TO TABLE NUMBER");
+
+
+// // while(a <= 10){
+// //     document.write(c);
+// //     document.write("<br>")
+// //     a++
+// //     var c = a*b;
+// // }
+        
+
+      //    IF STATMENT
+//     var a = prompt("ENTER A EVEN NUMBER");
+//        if(a % 2 ==0){
+//     window.location.href = "new.html";
+//  }
+    
+    
+            // IF ELSE STATMENT
+//  var a = prompt("ENTER EVEN NUMBER");
+//  if(a % 2 ==0){
+//        window.location.href ="new.html";
+//  }else{
+//        document.write("type even number");
+//  }
+
+
+       //   IF ELSE IF STATMENT
+// var a = prompt("ENTER NUMBER");
+// var b = prompt("ENTER NUMBER")
+//  if(a != b ){
+//       document.write("not equel")
+//  }else if(a == b){
+//        document.write("equel");
+//  }else{
+//     document.write("error")
+//  }
+
+//  
+
+// var a = 2 ;
+// while (a <= 100);{
+// document.write(a);
+// a++
+// }
+
+
+for(var a = 1; a <= 10; a++){
+    document.write(a);
+}
